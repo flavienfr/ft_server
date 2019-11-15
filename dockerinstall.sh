004555348e0ad461e21c1ecc5d9f0255fa58a5c8
flavienfr <44776722+flavienfr@users.noreply.github.com>
2019-11-15 14:50:17 +0100

    Delete dockerinstall.sh

--- dockerinstall.sh
+++
@@ -1,4 +0,0 @@
-mv /Users/froussel/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/Docker.qcow2 /sgoinfre/goinfre/Perso/froussel
-
-ln -s /sgoinfre/goinfre/Perso/froussel/Docker.qcow2 /Users/froussel/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/Docker.qcow2 
-
